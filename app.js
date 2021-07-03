d90c1d746274e3ad314ed8b7dc02b8f02eee4ace
buentellopc <buentellopc@gmail.com>
2021-07-03 08:56:27 -0500

    add joi validation

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const Joi = require("joi");
+const { campgroundSchema } = require("./schemas");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
+const { required } = require("joi");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -27,6 +30,20 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  // console.log(result);
+  console.log(error);
+  if (error) {
+    console.log(error.details);
+    console.log(error.details[0].message);
+    const msg = error.details[0].message;
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -51,11 +68,13 @@ app.get("/campgrounds/new", (req, res) => {
 // POST TO HANDLE THE CREATION
 app.post(
   "/campgrounds",
+  validateCampground,
 
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground) {
-      throw new ExpressError("Invalid Campground Data (post)", 400);
-    }
+    // if (!req.body.campground) {
+    //   throw new ExpressError("Invalid Campground Data (post)", 400);
+    // }
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -82,6 +101,7 @@ app.get(
 
 app.put(
   "/campgrounds/:id",
+  validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
