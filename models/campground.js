6996a572f00abb2ffa03fed852b5d79cf822c37a
buentellopc <buentellopc@gmail.com>
2021-07-07 14:14:23 -0500

    add post hook to delete reviews

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const CampgroundSchema = new Schema({
   title: String,
@@ -10,4 +11,20 @@ const CampgroundSchema = new Schema({
   reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
 });
 
-module.exports = mongoose.model("Campground", CampgroundSchema);
+// The route was using a method that belongs to a Model and returns a Query, probably that is why nothing whas shown here
+// CampgroundSchema.pre("updateOne", { document: true, query: false }, (doc) => {
+//   console.log(doc);
+//   console.log("this", this);
+// });
+
+CampgroundSchema.post("findOneAndDelete", async (doc) => {
+  // console.log(doc);
+  // console.log("this", this);
+  if (doc) {
+    await Review.deleteMany({ _id: { $in: doc.reviews } });
+  }
+});
+
+const Campground = mongoose.model("Campground", CampgroundSchema);
+
+module.exports = Campground;
