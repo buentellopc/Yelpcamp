3c266910c7ef702eb4bf89eb90bdfd9ed7155cbf
Buentello <buentellopc@gmail.com>
2021-07-26 18:11:34 -0500

    add register form

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+// router
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -51,7 +53,7 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
-// before or after session???
+// after session
 app.use(passport.initialize());
 app.use(passport.session());
 // authenticate is added by passport-local-mongoose
@@ -75,8 +77,9 @@ app.get("/fakeUser", async (req, res) => {
 });
 
 // routes
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use("/", userRoutes);
 
 // app.get("/makecampgrounds", async (req, res) => {
 //   const camp = new Campground({
