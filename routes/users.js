0319c35fb58f0373bd7ed20b2606df393efac428
Buentello <buentellopc@gmail.com>
2021-08-03 13:14:59 -0500

    add logout route

--- routes/users.js
+++ routes/users.js
@@ -46,4 +46,10 @@ router.post(
   }
 );
 
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success", "Goodbye!");
+  res.redirect("/campgrounds");
+});
+
 module.exports = router;
