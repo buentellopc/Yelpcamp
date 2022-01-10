db226ea5b6d0c5b22f82a53f12c37bed1fa2dc47
Buentello <buentellopc@gmail.com>
2022-01-10 15:28:38 -0600

    Minor change to session/cookies

--- app.js
+++ app.js
@@ -47,11 +47,13 @@ app.use(methodOverride("_method"));
 app.use(mongoSanitize());
 
 const sessionConfig = {
-  httpOnly: true,
+  name: "session",
   secret: "supersecret",
   resave: false,
   saveUninitialized: true,
   cookie: {
+    httpOnly: true,
+    //    secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
