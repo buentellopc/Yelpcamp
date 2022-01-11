9474001812751d6851e365b7e92133429b7d3ba4
Buentello <buentellopc@gmail.com>
2022-01-11 12:31:59 -0600

    Ready to deploy

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
+const MongoStore = require("connect-mongo")(session);
 
 // router
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
 
-// mongodb://localhost:27017/yelp-camp
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -49,9 +49,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(mongoSanitize());
 
+const secret = process.env.SECRET || "thisshouldbeasecret";
+
+const store = new MongoStore({
+  url: dbUrl,
+  secret,
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+  console.log("SESSION STORE ERROR", e);
+});
+
 const sessionConfig = {
+  store,
   name: "session",
-  secret: "supersecret",
+  secret,
   resave: false,
   saveUninitialized: true,
   cookie: {
