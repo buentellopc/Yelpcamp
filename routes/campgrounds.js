a0763ba3e60a79e206a557ed0562de843a24a678
Carlos Buentello <85374941+buentellopc@users.noreply.github.com>
2023-04-18 16:40:58 -0600

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 
 const Campground = require("../models/campground");
-const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middlewareNotVercel");
 const multer = require("multer");
 const { storage } = require("../cloudinary");
 const upload = multer({ storage });
